cb5f6374e985c277f7d9cdee9bb0f4df63ab5758
Mihai Bercu <mihaiberq@gmail.com>
2017-02-21 19:04:08 +0000

    token verification

--- src/index.js
+++ src/index.js
@@ -3,24 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import cookie from 'react-cookie';
+import axios from 'axios';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 import LoginContainer from './containers/LoginContainer';
 import reducer from './redux/modules/user';
 import './index.css';
 
-function isAuth(nextState, replace) {
-  if (!cookie.load('token')) {
-    replace({
-      pathname: '/login',
-      state: { nextPathname: nextState.location.pathname },
-    });
-  }
-}
-
-
 const store = createStore(reducer, { token: cookie.load('token') || '' });
 
+function isAuth() {
+  axios.get('https://project-run.herokuapp.com/test',
+  { headers: { Authorization: store.getState().token } }).then(res => console.log(res));
+}
 
 ReactDOM.render(
   <Provider store={store}>
