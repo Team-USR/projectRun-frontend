ff3a7ca07ff1a2f9e64c3d3eb08998406a9e320d
Mihai Bercu <mihaiberq@gmail.com>
2017-02-23 15:08:57 +0000

    change middleware

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
-import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
+import { syncHistoryWithStore, routerReducer, routerMiddleware, push } from 'react-router-redux';
 import App from './App';
 import LoginContainer from './containers/LoginContainer';
 import reducer from './redux/modules/user';
@@ -13,16 +13,14 @@ const store = createStore(
   combineReducers({
     reducer,
     routing: routerReducer,
-  }), applyMiddleware(routerMiddleware));
+  }), applyMiddleware(routerMiddleware(browserHistory)));
 
 const history = syncHistoryWithStore(browserHistory, store);
 
 
 function isAuth() {
   if (store.getState().reducer.token === '') {
-    console.log(store.getState());
-    store.dispatch(history.push('/login'));
-    window.location.reload();
+    store.dispatch(push('/login'));
   }
 }
 
