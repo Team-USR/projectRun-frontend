045ff791fb73a5207fcc5850bc5e9c0df1d5e10a
Ionut Burechita <ionut.burechita@kcl.ac.uk>
2017-03-14 23:52:57 +0000

    Add a simple form with radio-buttons for UserType in Settings

--- src/components/Settings/SettingsPage.js
+++ src/components/Settings/SettingsPage.js
@@ -4,7 +4,17 @@ export default class SettingsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedOption: 'teacher',
     };
+  }
+
+  onChange(e) {
+    const name = e.target.name;
+    if (name === 'teacher') {
+      this.setState({ selectedOption: 'teacher' });
+    } else if (name === 'student') {
+      this.setState({ selectedOption: 'student' });
+    }
   }
 
   renderSettings() {
@@ -12,6 +22,22 @@ export default class SettingsPage extends Component {
     return (
       <div>
         <h1><b>Panel</b></h1>
+        <form action="" >
+          <input
+            type="radio"
+            name="teacher"
+            value="teacher"
+            onChange={e => this.onChange(e)}
+            checked={this.state.selectedOption === 'teacher'}
+          /> TEACHER<br />
+          <input
+            type="radio"
+            name="student"
+            value="student"
+            onChange={e => this.onChange(e)}
+            checked={this.state.selectedOption === 'student'}
+          /> STUDENT<br />
+        </form>
       </div>
     );
   }
