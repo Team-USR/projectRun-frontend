acace27110c97eab799744808042b60ff44cb5c1
Mihai Bercu <mihaiberq@gmail.com>
2017-03-17 18:08:42 +0100

    quiz editor can display fill in the gaps exercises

--- src/quizzes/Cloze/ClozeSentence.js
+++ src/quizzes/Cloze/ClozeSentence.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { GAP_MATCHER } from '../../constants';
 
-export default function ClozeSentence(props) {
-  const words = props.question.split(/\s/);
-  if (!words[words.length - 1].match(/[a-zA-Z0-9]+\./)) {
-    words.push('.');
+export default class ClozeSentence extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      resultsState: true,
+      reviewState: false,
+    };
   }
 
-  const tooltip = <Tooltip id="tooltip">{props.hint || 'No hints!'}</Tooltip>;
+  render() {
+    const words = this.props.question.split(/\s/);
+    const gapsCopy = this.props.gaps;
+    if (!words[words.length - 1].match(/[A-zÀ-ÿ0-9]+\./)) {
+      words.push('.');
+    }
 
-  return (
-    <li key={props.index}>
-      {words.map(w => (w.match(/{\d}/) ?
-      (<OverlayTrigger key={Math.floor(Math.random() * 10000)} placement="top" overlay={tooltip}>
-        <input type="text" maxLength="12" />
-      </OverlayTrigger>) : `${w} `))}
-    </li>
-  );
+    return (
+      <li key={this.props.index}>
+        {words.map((w) => {
+          if (!w.match(GAP_MATCHER)) {
+            return `${w} `;
+          }
+          const gap = gapsCopy.slice(0, 1)[0];
+          const tooltip = <Tooltip id={`tooltip-${gap.id}`}>{gap.hint.hint_text || 'No hints!'}</Tooltip>;
+          return (
+            <OverlayTrigger key={`gap-${gap.id}`} placement="top" overlay={tooltip}>
+              <input type="text" maxLength="12" value={this.state.resultsState ? gap.gap_text : ''} readOnly={this.state.resultsState} />
+            </OverlayTrigger>
+          );
+        })}
+      </li>
+    );
+  }
 }
 
 ClozeSentence.propTypes = {
   index: React.PropTypes.number.isRequired,
   question: React.PropTypes.string.isRequired,
-  hint: React.PropTypes.string.isRequired,
+  gaps: React.PropTypes.arrayOf(React.PropTypes.shape({
+    id: React.PropTypes.number,
+    gap_text: React.PropTypes.string,
+    hint: React.PropTypes.shape({
+      id: React.PropTypes.number,
+      hint_text: React.PropTypes.string,
+    }),
+  })).isRequired,
 };
