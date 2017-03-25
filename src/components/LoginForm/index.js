38318edb2734d60903e68eb33f1102f8057d2160
K1502818 <vlad.niculescu@kcl.ac.uk>
2017-03-25 22:41:05 +0000

    student-teacher toggle in the login page

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { ControlLabel, form, FormControl, Button, ButtonGroup } from 'react-bootstrap';
+import { ControlLabel, form, FormControl, Button, ButtonGroup, Col } from 'react-bootstrap';
+import cookie from 'react-cookie';
+import Toggle from 'react-toggle';
+import { STUDENT, TEACHER } from '../../constants';
 
 export default function LoginForm(props) {
   let error;
   if (props.loginError) {
     error = <h3 className="invalidHeader">{props.loginError}</h3>;
   }
+  let isTeacher = false;
+  if (cookie.load('userType') !== undefined) {
+    isTeacher = cookie.load('userType');
+  }
+
   return (
     <div className="loginPage">
       <h1 className="welcomeTitle" id="title">Welcome!</h1>
@@ -29,18 +37,46 @@ export default function LoginForm(props) {
             onChange={props.handlePasswordChange}
           />
         </form>
+
         <div className="loginButtonWrapper">
-          <ButtonGroup>
-            <Button
-              type="submit"
-              onClick={props.submitLogin}
-            >
+          <Col md={12}>
+            <Col md={6}>
+              <form style={{ width: 300, display: 'inline-flex' }}>
+                <div style={{ display: 'inline-block', lineHeight: 10 }}>
+                  <h5>Student</h5>
+                </div>
+                <div style={{ marginTop: 5, marginLeft: 5, marginRight: 5 }}>
+                  <Toggle
+                    defaultChecked={isTeacher}
+                    icons={false}
+                    onChange={() => {
+                      if (isTeacher === false) {
+                        cookie.save('userType', TEACHER);
+                        isTeacher = true;
+                      } else {
+                        cookie.save('userType', STUDENT);
+                        isTeacher = false;
+                      }
+                    }}
+                  />
+                </div>
+                <h5>Teacher</h5>
+              </form>
+            </Col>
+            <Col md={6}>
+              <ButtonGroup>
+                <Button
+                  type="submit"
+                  onClick={props.submitLogin}
+                >
               Login
             </Button>
-            <LinkContainer to={'/signup'}>
-              <Button>Sign-up</Button>
-            </LinkContainer>
-          </ButtonGroup>
+                <LinkContainer to={'/signup'}>
+                  <Button>Sign-up</Button>
+                </LinkContainer>
+              </ButtonGroup>
+            </Col>
+          </Col>
         </div>
       </div>
     </div>
--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -1,11 +1,13 @@
+import cookie from 'react-cookie';
 import { connect } from 'react-redux';
 import { TEACHER } from '../constants';
 import { NavBar } from '../components/UserAccount/';
 import { logoutUser } from '../redux/modules/user';
 
+
 const mapStoreToProps = store => ({
   userType: store.auth.userType,
-  userTypeClass: store.auth.userType === TEACHER ? 'fa-university'
+  userTypeClass: (store.auth.userType || cookie.load('userType')) === TEACHER ? 'fa-university'
     : 'fa-graduation-cap',
 });
 
