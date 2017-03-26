fc50879113d06984c0f7942d0491be9f14cc6c72
Mihai Bercu <mihaiberq@gmail.com>
2017-03-26 18:42:49 +0100

    Added some styling

--- src/createQuizzes/Cloze/ClozeList.js
+++ src/createQuizzes/Cloze/ClozeList.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 
 export default function ClozeList(props) {
   return (
-    <ol className="cloze-sentences">
-      {props.questions.map(q => (
-        <li key={q.no}>{q.question}
-          <Button onClick={() => props.removeQuestion(q)}>x</Button>
-        </li>
-      ))}
-    </ol>
+    <Col md={12}>
+      <ul className="cloze-sentences">
+        {props.questions.map(q => (
+          <div>
+            <li style={{ paddingTop: 5 }} key={q.no}>
+              <Col md={11}>
+                <p className="text-center">{q.question}</p>
+              </Col>
+              <Col md={1}>
+                <Button onClick={() => props.removeQuestion(q)}>x</Button>
+              </Col>
+            </li>
+            <hr />
+          </div>
+        ))}
+      </ul>
+    </Col>
   );
 }
 
