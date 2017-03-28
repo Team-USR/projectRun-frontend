c2f8cc7480f2456d66c9afc64652eac5cbabb8b1
Mihai Bercu <mihaiberq@gmail.com>
2017-03-28 02:34:32 +0100

    Tests working

--- tests/student/02findClass.js
+++ tests/student/02findClass.js
@@ -13,8 +13,47 @@ module.exports = {
       .waitForElementVisible('h1[class=welcome_message]', 2000)
       .expect.element('h1[class=welcome_message]').text.to.contain('Welcome, Nightwatch!');
   },
-  'Find a class': (browser) => {
+  'Go to a class': (browser) => {
+    browser //eslint-disable-line
+      .click('a[id=class-nav]')
+      .pause(1000)
+      .waitForElementVisible('div[class=groupPanelWrapper]', 2000)
+      .waitForElementVisible('div[class=quizList]', 1000)
+      .useXpath()
+      .expect.element('//button[contains(text(), "Nightwatch class")]').to.be.present;
+
     browser
-      .end();
+      .useCss()
+      .setValue('input[id=searchBar]', 'nightwatch class')
+      .waitForElementVisible('li[class=classesNav]', 2000)
+      .useXpath()
+      .click('//li[contains(@class, "classesNav")]//button[contains(text(), "Nightwatch class")]')
+      .pause(2000)
+      .useCss()
+      .waitForElementVisible('div[class="line-chart-container"] div[class="recharts-wrapper"]', 1000);
+  },
+
+  'Find a class': (browser) => {
+    browser //eslint-disable-line
+      .pause(1000)
+      .useXpath()
+      .click('//button[contains(text(), "Join a class")]')
+      .useCss()
+      .waitForElementVisible('input[id=searchBarClasses]', 2000)
+      .expect.element('div[class=pendingClasses]').to.be.present;
+
+    browser //eslint-disable-line
+      .setValue('input[id=searchBarClasses]', 'ouhqwbiehpqwe')
+      .pause(2500)
+      .expect.element('span[class="glyphicon-plus"]').not.to.be.present;
+
+    browser //eslint-disable-line
+      .clearValue('input[id=searchBarClasses]')
+      .setValue('input[id=searchBarClasses]', 'software')
+      .pause(2500)
+      .useXpath()
+      .expect.element('//button[contains(text(), "Software Engineering")]').to.be.present;
+
+    browser.end();
   },
 };
